4b388548013e15996c341061a4bb7a3cbeb82327
ashishborkute7 <102751812+ashishborkute7@users.noreply.github.com>
2022-04-12 19:51:59 +0530

    Update settings.py

--- Librarymanagement/settings.py
+++ Librarymanagement/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["library-manage-project.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["library-manage-project.herokuapp.com", "127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://library-manage-project.herokuapp.com'
